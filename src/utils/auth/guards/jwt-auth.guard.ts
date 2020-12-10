6acaed283c105d72f774c049df31a24a8d0a5d8e
enzo <enzo@enzos-iMac-Pro.homenet.telecomitalia.it>
2020-12-11 00:54:05 +0100

    fix JwtAuthGuard

--- src/utils/auth/guards/jwt-auth.guard.ts
+++ src/utils/auth/guards/jwt-auth.guard.ts
@@ -1,15 +1,20 @@
+import { SessionService } from '@/utils/session/session.service';
 import {
   ExecutionContext,
   ForbiddenException,
   Injectable,
+  Logger,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  private readonly logger = new Logger(JwtAuthGuard.name);
+
   constructor(
-    //private readonly usersService: UserService,
+    private readonly sessionService: SessionService,
     private readonly jwtService: JwtService,
   ) {
     super();
@@ -22,67 +27,20 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
         .getRequest()
         .headers.authorization.split(' ')[1];
 
-      const user = this.jwtService.verify(token);
-      //console.log(user);
-      //if (user === null) return false;
+      const pyload = this.jwtService.verify(token);
+      const session = await this.sessionService.findOne(
+        new Types.ObjectId(pyload.sessionId),
+      );
 
-      return true;
+      if (session !== null) {
+        return true;
+      }
+
+      this.logger.warn('Auth fail session not valid');
+      return false;
     } catch (e) {
+      this.logger.error(e);
       throw new ForbiddenException(e.message);
     }
   }
 }
-
-/*
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  SetMetadata,
-  UseGuards,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Reflector } from '@nestjs/core';
-import { JwtService } from '@nestjs/jwt';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(
-    private reflector: Reflector,
-    private readonly jwtService: JwtService,
-    @InjectModel('User') private service: Model<User>,
-  ) {
-    super();
-  }
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    try {
-      const roles = this.reflector.get<string[]>('roles', context.getHandler());
-      const token = context
-        .switchToHttp()
-        .getRequest()
-        .headers.authorization.split(' ')[1];
-      const user = await this.service.findById(
-        this.jwtService.verify(token)._id,
-      );
-
-      if (user == null) throw new UnauthorizedException();
-
-      if (!roles) return true;
-
-      let state = false;
-      roles.forEach((role) => {
-        if (user.roles.includes(role)) state = true;
-      });
-      return state;
-    } catch (ExceptionsHandler) {
-      throw new UnauthorizedException();
-    }
-  }
-}
-
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
-*/
